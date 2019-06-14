cf46b1b71b0121b48e073b2bf1f9a0f8e8eeabd8
prajwalmore5 <39994719+prajwalmore5@users.noreply.github.com>
2019-06-14 09:45:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+blue=(0,0,255)
+green=(0,255,0)
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+pygame.draw.rect(gameDisplay, green,[300,400,40,40])
+pygame.draw.circle(gameDisplay, red,[200,300],40)
+pygame.draw.line(gameDisplay, blue,(100,100),(500,500),10)
+pygame.display.update()
+
